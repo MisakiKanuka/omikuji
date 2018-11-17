46862b429b56a209b6072f464efb08d3740b2f4c
Kanuka <kanukamisaki@icloud.com>
2018-11-17 18:21:38 +0900

    First commit 全然分からない！！！

---
+++ omikuji.py
@@ -0,0 +1,14 @@
+input
+random  # ランダムに
+
+omikuji = ["大吉", "吉", "凶"]
+
+result = omikuji
+
+print(result)
+
+# omikuji = ["大吉", "吉", "凶"] # 結果リストをまとめて定義する
+
+# result = ??? # ランダムに1つ取得するのはちょっと難しそうだ
+
+# print(f"今日の運勢は... {result}") # これは簡単
